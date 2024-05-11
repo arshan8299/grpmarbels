4273c8af3628d50237f60cb7e6c451d134246265
Saksham <sakshamverma7844@gmail.com>
2024-05-11 22:15:08 +0530

    aboutus page done

---
+++ src/constants/about.js
@@ -0,0 +1,15 @@
+export const ABOUTUS = "About Us"
+export const ABOUT_CONTENT = [
+  {
+    image: "/our_mission.jpg",
+    aboutTitle:"Our Mission",
+    aboutContent:
+      "As artisans in the marble industry, we've been dedicated to crafting exquisite marble carvings, sculptures, temples, and statues since our inception. With a wealth of satisfied clients spanning years, our work is renowned for its intricate designs, impeccable craftsmanship, and enduring quality.",
+  },
+  {
+    image: "/our_vision.jpg",
+    aboutTitle:"Our Vision",
+    aboutContent:
+      "Our objective is to design top-tier marble creations that not only captivate visually but also propel your business forward. We concentrate on crafting pieces that not only exude beauty but also contribute to increased sales and business expansion. By incorporating our marble creations into your space, you can enhance its allure and amplify your ability to attract a broader clientele.",
+  },
+]
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,8 +1,24 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import { ABOUTUS, ABOUT_CONTENT } from '../../constants/about'
 
 const About = () => {
   return (
-    <Box>About</Box>
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Typography alignSelf="center" marginTop="2rem" fontSize={50} fontWeight={500}>{ABOUTUS}</Typography>
+      <Box sx={{ padding: { xs: "1rem", md: "2rem 4rem" } }}>
+        {ABOUT_CONTENT?.map((item, index) => (
+          <Box sx={{ display: "flex", gap: "1.5rem", marginBlock: "3rem", flexDirection: { xs: "column", md: index % 2 === 0 ? "row" : "row-reverse" }, justifyContent: "center" }} key={index}>
+            <Box sx={{ width: { sm: "100%", md: "500px" }, height: { xs: "250px", sm: "500px", md: "400px" } }}>
+              <img style={{ width: "100%", height: "100%", objectFit: "cover" }} src={item.image} alt="about-us" />
+            </Box>
+            <Box sx={{ width: { xs: "100%", md: "500px" }, height: "100%", display: "flex", flexDirection: "column", alignSelf: { md: "center" } }}>
+              <Typography alignSelf="center" marginBlock="1rem" fontSize={30} fontWeight={500} >{item.aboutTitle}</Typography>
+              <Typography textAlign="center" marginBlock="1rem" fontSize={18} lineHeight="1.5" fontWeight={400} >{item.aboutContent}</Typography>
+            </Box>
+          </Box>
+        ))}
+      </Box>
+    </Box>
   )
 }
 
